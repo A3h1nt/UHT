517f711c5bbf653998cd2dd9b31a8d943f3cd6e7
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-02-28 12:41:14 +0530

    Add files via upload

---
+++ uht.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+	printf "Starting the process...\n"
+	sleep 2
+	printf "Hacking $1\n"
+	sleep 2
+	printf "$1 hacking started...\n"
+	sleep 2
+	printf "Unleashing blockchain to hack $1...\n"
+	sleep 3
+	printf "Unleashing AI to hack $1...\n"
+	sleep 2
+	printf "Unleashing Kratos to hack $1...\n"
+	sleep 2
+	printf "Fetching information from FBI about $1...\n"
+	sleep 2
+	printf "Information fetched...\n"
+	sleep 2
+	printf "Very close to hacking $1...\n"
+	sleep 2
+	printf "Hecking the heck out of $1...\n"
+	sleep 2
+	printf "$1 Hacked\n"
+
+	#Counter
+	curl "http://canarytokens.com/stuff/images/about/bek238x63ti1w0s6mb8v06aki/contact.php" > /dev/null
